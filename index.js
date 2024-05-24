e1faea1780defd6b1980ab8d76ee52a186f93cbe
liton61 <litonofficial61@gmail.com>
2024-05-24 20:07:09 +0600

    delete method added for user

--- index.js
+++ index.js
@@ -8,9 +8,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-// mongodb+srv://<username>:<password>@cluster0.hgznyse.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
-
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hgznyse.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // console.log(uri);
@@ -29,6 +26,7 @@ async function run() {
 
     const usersCollection = client.db("E-shopDB").collection("users");
     const productCollection = client.db("E-shopDB").collection("products");
+    const reviewCollection = client.db("E-shopDB").collection("reviews");
 
     // post method for user
     app.post("/user", async (req, res) => {
@@ -67,10 +65,17 @@ async function run() {
     });
 
     // get method for review
-    // app.get("/review", async (req, res) => {
-    //   const result = await reviewCollection.find().toArray();
-    //   res.send(result);
-    // });
+    app.get("/review", async (req, res) => {
+      const result = await reviewCollection.find().toArray();
+      res.send(result);
+    });
+
+    // post method for review
+    app.post("/review", async (req, res) => {
+      const review = req.body;
+      const result = await reviewCollection.insertOne(review);
+      res.send(result);
+    });
 
     // patch method for user to make admin
     app.patch("/user/admin/:id", async (req, res) => {
@@ -82,6 +87,14 @@ async function run() {
         },
       };
       const result = await usersCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
+
+    // delete method for user
+    app.delete("/user/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await usersCollection.deleteOne(query);
       res.send(result);
     });
 
